50e28afc708bcd24860645628fc9ce86b738d842
ekathuria <e@ekathuria.dev>
2022-03-16 22:08:37 -0400

    Convert wav files to mp3

--- tonegenerator.js
+++ tonegenerator.js
@@ -1,27 +1,24 @@
- function hashCode(string, size){
-    let hash = 0;
-    if (string.length == 0) return hash;
-    for (let i = 0; i < string.length; i++) {
-      const letter = string.charCodeAt(i);
-      hash = ((hash << 5) - hash) + letter;
-      hash = hash & hash; // Convert to 32bit integer
-    }
-    return Math.abs(hash) % size ;
-  }
+// function hashCode(string, size) {
+//   let hash = 0;
+//   if (string.length == 0) return hash;
+//   for (let i = 0; i < string.length; i++) {
+//     const letter = string.charCodeAt(i);
+//     hash = ((hash << 5) - hash) + letter;
+//     hash = hash & hash; // Convert to 32bit integer
+//   }
+//   return Math.abs(hash) % size;
+// }
 
-  const words = ['This', 'example', 'shows', 'how', 'an', 'extension', 'can', 'toggle', 'the', 'muted', 'state', 'for', 'a', 'given', 'tab']
+// const words = ['This', 'example', 'shows', 'how', 'an', 'extension', 'can', 'toggle', 'the', 'muted', 'state', 'for', 'a', 'given', 'tab']
 
-  for(let word of words) {
-    let note = hashCode(word, 5)
-    let music = new Audio({
-      loop: false,
-      volume: 1,
-      src: [`/piano_samples/${note}.wav`]
-    })
-    music.play()
-    setTimeout(() => {}, 1000)
-  }
-
-  
-var myAudioObject = new Audio();
-
+// for (let word of words) {
+//   let note = hashCode(word, 5)
+let music = new Audio({
+  loop: false,
+  volume: 1,
+  autoplay: true,
+  src: [`/piano_samples/${note}.mp3`]
+})
+music.play()
+  // setTimeout(() => { }, 1000)
+// }
